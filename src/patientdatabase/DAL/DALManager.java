469858a228167eee83206ed970f6f1ac0e9938ce
jeppeml <jeppeml@users.noreply.github.com>
2016-11-15 12:41:10 +0100

    Added read from CSV

--- src/patientdatabase/DAL/DALManager.java
+++ src/patientdatabase/DAL/DALManager.java
@@ -5,6 +5,13 @@
  */
 package patientdatabase.DAL;
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import patientdatabase.BE.Patient;
+
 /**
  *
  * @author jeppjleemoritzled
@@ -25,5 +32,35 @@ public class DALManager
     
     private DALManager()
     {
+    }
+    
+    public List<Patient> getAllPatients()
+    {
+        List<Patient> returnList = new ArrayList<>();
+        
+        try(BufferedReader CSVFile = new BufferedReader(
+            new FileReader("patients.csv")))
+        {
+            CSVFile.readLine(); // SKIP FIRST LINE HEADER
+            String line = CSVFile.readLine();
+            while(line!=null)
+            {
+                String[] dataArray = line.split(",");
+                returnList.add(new Patient(
+                                Integer.parseInt(dataArray[0]), 
+                                dataArray[1], 
+                                dataArray[2], 
+                                dataArray[3]));
+                
+                line = CSVFile.readLine(); // Read next line
+            }
+        }
+        catch(IOException ioe)
+        {
+            System.out.println(ioe);
+            return null;
+        }
+        return returnList;
+        
     }
 }
